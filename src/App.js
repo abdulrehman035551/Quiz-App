9fef063f428d1c5aae9b85ccfd5aa8d03653c0f4
Abdul <abdulrehman035551@gmail.com>
2022-12-03 17:55:21 +0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Test  from "./component/test";
+
+function App() {
+  return (
+    <div>
+     <h3>hello</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let a:number=80
+function add(num:number)
+{
+    console.log(num)
+}
+add(3)
